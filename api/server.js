ea4ed40de27bc6313d296bd669dbf6ef51f6bfa9
Jonathan-Majors <izzymajors@gmail.com>
2021-03-12 09:55:37 -0600

    package.json added libraries

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+
+const actionRouter = require('./actions/actions-router')
+const projectRouter = require('./projects/projects-router')
+
 const server = express();
 
 // Complete your server here!
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(cors());
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
